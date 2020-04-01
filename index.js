9754057c627af4867412adc762ffe93b4d21fe65
Martin <martintonts@gmail.com>
2020-04-01 13:56:41 +0300

    update on logic

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 'use strict';
 
 const express = require('express');
-const request = require("request");
+const got = require('got');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-const overpassQuery = "https://overpass-api.de/api/interpreter?data=%2F%2A%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9Cpark_ride%3D%2A%20in%20estonia%E2%80%9D%0A%2A%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20fetch%20area%20%E2%80%9Cestonia%E2%80%9D%20to%20search%20in%0Aarea%283600079510%29-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cpark_ride%3D%2A%E2%80%9D%0A%20%20node%5B%22park_ride%22%3D%22yes%22%5D%28area.searchArea%29%3B%0A%20%20way%5B%22park_ride%22%3D%22yes%22%5D%28area.searchArea%29%3B%0A%20%20relation%5B%22park_ride%22%3D%22yes%22%5D%28area.searchArea%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B";
-// App
+
 const app = express();
 
-app.get('/', (req, res) => {
-  request.get(overpassQuery).pipe(res);
+async function fetch_data() {
+  return (await got('https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0A*%2F%0A[out%3Ajson][timeout%3A25]%3B%0A%2F%2F%20fetch%20area%20%E2%80%9Cestonia%E2%80%9D%20to%20search%20in%0Aarea(3600079510)-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A(%0A%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cpark_ride%3D*%E2%80%9D%0A%20%20node[%22amenity%22%3D%22parking%22][%22park_ride%22%3D%22yes%22](area.searchArea)%3B%0A%20%20way[%22amenity%22%3D%22parking%22][%22park_ride%22%3D%22yes%22](area.searchArea)%3B%0A%20%20relation[%22amenity%22%3D%22parking%22][%22park_ride%22%3D%22yes%22](area.searchArea)%3B%0A%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22bicycle%20parking%22%E2%80%9D%0A%20%20node[%22amenity%22%3D%22bicycle_parking%22](area.searchArea)%3B%0A%20%20way[%22amenity%22%3D%22bicycle_parking%22](area.searchArea)%3B%0A%20%20relation[%22amenity%22%3D%22bicycle_parking%22](area.searchArea)%3B%0A)%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B%0A', {responseType: 'json', resolveBodyOnly: true}));
+}
+
+let data;
+
+app.get('/', async (req, res) => {
+  res.send(data);
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while (!data || !data.elements || !data.elements.length) {
+    try {
+      console.log("Overpass API status:");
+      console.log((await got('https://overpass-api.de/api/status', {resolveBodyOnly: true})));
+      console.log("Trying to fetch data...");
+      data = await fetch_data();
+      if (!data.elements.length) console.log(data.remark);
+    } catch(error) {
+      console.error(error.response.body);
+    }
+    await sleep(10000);
+  }
+
+  app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
+}
+
+main();
+
