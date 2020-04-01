bafe34582e0a6977b3a5067d1385b96c0334d67b
Martin <martintonts@gmail.com>
2020-04-01 16:08:42 +0300

    log client connecting

--- index.js
+++ index.js
@@ -14,9 +14,14 @@ async function fetch_data() {
   return (await got('https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0A*%2F%0A[out%3Ajson][timeout%3A25]%3B%0A%2F%2F%20fetch%20area%20%E2%80%9Cestonia%E2%80%9D%20to%20search%20in%0Aarea(3600079510)-%3E.searchArea%3B%0A%2F%2F%20gather%20results%0A(%0A%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cpark_ride%3D*%E2%80%9D%0A%20%20node[%22amenity%22%3D%22parking%22][%22park_ride%22%3D%22yes%22](area.searchArea)%3B%0A%20%20way[%22amenity%22%3D%22parking%22][%22park_ride%22%3D%22yes%22](area.searchArea)%3B%0A%20%20relation[%22amenity%22%3D%22parking%22][%22park_ride%22%3D%22yes%22](area.searchArea)%3B%0A%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9C%22bicycle%20parking%22%E2%80%9D%0A%20%20node[%22amenity%22%3D%22bicycle_parking%22](area.searchArea)%3B%0A%20%20way[%22amenity%22%3D%22bicycle_parking%22](area.searchArea)%3B%0A%20%20relation[%22amenity%22%3D%22bicycle_parking%22](area.searchArea)%3B%0A)%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B%0A', {responseType: 'json', resolveBodyOnly: true}));
 }
 
+function getNow() {
+  return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
+
 let data;
 
 app.get('/', async (req, res) => {
+  console.log(getNow() + ' - Client connected');
   res.send(data);
 });
 
